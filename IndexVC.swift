8cc9bbd38a8d4eca065e72cf8774e7c9d5764c1f
aybek can kaya <aybekcankaya@icloud.com>
2017-06-17 21:08:46 +0300

    init

---
+++ IndexVC.swift
@@ -0,0 +1,35 @@
+//
+//  IndexVC.swift
+//  YSBannerComponent
+//
+//  Created by aybek can kaya on 17/06/2017.
+//  Copyright © 2017 aybek can kaya. All rights reserved.
+//
+
+import UIKit
+
+class IndexVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
